a0444b7d37c3d4a68de12eb67b57526ba31e03b3
andrescol24 <andresmil24@hotmail.com>
2021-05-24 12:10:44 -0500

    HTTP methods and receive object in the body

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body} from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -6,5 +6,21 @@ export class TasksController {
     @Get()
     getTasks(): string {
         return "Returning tasks";
+    }
+
+    @Post()
+    addTask(@Body() task): string {
+        console.log(task);
+        return "Added";
+    }
+
+    @Put()
+    editTask(): string {
+        return "Edited";
+    }
+
+    @Delete()
+    deleteTask() : String {
+        return "deleted";
     }
 }
